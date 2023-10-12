97b79a5cb3a1e041c294a000257015b45f7044d3
robinsonirua <robinsonirua1@gmail.com>
2023-10-12 13:41:49 -0700

    shell redirections

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+  
+/**
+ * void print_alphabet_x10(void) - entry point
+ * Return 0: return void
+ */
+          
+void print_alphabet_x10(void)
+{         
+int a;   
+char i; 
+for (a = 0; a < 1;  a++)
+{            
+        for (i = 'a'; i <='z'; i++)
+        {
+        _putchar(a);
+        }
+        _putchar('\n');
+}
+}
