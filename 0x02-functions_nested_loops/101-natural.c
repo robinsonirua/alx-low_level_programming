e5b76328c58e0979388d51d9cb5a9f0ffa5883b4
robinsonirua <robinsonirua1@gmail.com>
2023-10-12 15:26:07 -0700

    shell redirections

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,22 +1,42 @@
-#include "main.h"
-
-
+#include <stdio.h>
 /**
- * main - check the code.
+ * main - compute and prints the sum of all the multiples
+ * of 3 or 5 below
+ * Return: always 0 (Success)
  *
- * Return: Always 0.
+ * FUNCTIONALITY *
+ *
+ * 1. Declare variables
+ * 2. Initialize variables
+ * 3. Loop through numbers 0 to 1024
+ * 4. If the number is divisible by 3, add it to sum3
+ * 5. If the number is divisible by 5, add it to sum5
+ * 6. Add sum3 and sum5 together
+ * 7. Print the result
+ *
+ * Time Complexity: O(n)
  */
-
-
 int main(void)
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
+	unsigned long int sum3, sum5, sum;
+	int i;
+
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; ++i)
+	{
+		if ((i % 3) == 0)
+		{
+			sum3 = sum3 + i;
+		}
+		else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
+		}
+	}
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
 	return (0);
 }
-
