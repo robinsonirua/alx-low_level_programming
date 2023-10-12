c84dc8a6fba5d6635cb66624cf323c2ef9a0ab7d
robinsonirua <robinsonirua1@gmail.com>
2023-10-12 14:43:54 -0700

    shell redirections

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,17 @@
 #include "main.h"
 /**
- * main - check the code.
- * Return: Always 0.
+ * _isalpha - Checks charcter
+ * @c: The charceter
+ * Return: 1 for all
  */
-int main(void)
-{
-	int r;
 
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(108);
-	_putchar(r + '0');
-	r = _isalpha(';');
-	_putchar(r + '0');
-	_putchar('\n');
+int _isalpha(int c)
+
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
 	return (0);
 }
 
