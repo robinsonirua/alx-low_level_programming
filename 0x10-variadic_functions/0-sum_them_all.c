46840c6e786c1cd3488fc0a83eb219160877a844
robinsonirua <robinsonirua1@gmail.com>
2023-11-09 18:44:21 -0800

    shell redirections

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,30 +1,28 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
-  * sum_them_all - ...
-  * @n: ...
-  *
-  * Return: ...
-  */
+ * sum_them_all - Entry point
+ * Desc: Entry
+ *@n: int value
+ * Return: Always 0 (Success)
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
-	unsigned int i = 0;
+	va_list valist;
 	int sum = 0;
+	unsigned int i;
 
-	if (n != 0)
+	if (n == 0)
 	{
-		va_start(args, n);
-
-		while (i < n)
-		{
-			sum += va_arg(args, int);
-			i++;
-		}
-
-		va_end(args);
-		return (sum);
+		return (0);
 	}
-
-	return (0);
+	va_start(valist, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		sum = sum + va_arg(valist, int);
+	}
+	va_end(valist);
+	return (sum);
 }
